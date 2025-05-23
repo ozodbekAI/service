b730b02dde406feba12fe2b23d4bab234446bb2d
ozodbekAI <ozodbekakramjonov7@gmail.com>
2025-05-24 03:06:10 +0500

    add static

--- config/settings.py
+++ config/settings.py
@@ -205,7 +205,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/ec2-user/service/staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path(BASE_DIR) / 'media'
