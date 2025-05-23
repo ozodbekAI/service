a4c0807da8cdec424b45a0fbfa837a1ed8467445
ozodbekAI <ozodbekakramjonov7@gmail.com>
2025-05-24 04:35:36 +0500

    add data

--- users/serializers.py
+++ users/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth.hashers import make_password
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'phone', 'username', 'company_name', 'role', 'is_legal', "password"]
+        fields = ['id', 'email', 'phone', 'username', 'company_name', 'role', 'is_legal', "password", "date_joined"]
         extra_kwargs = {
             'password': {'write_only': True},
         }
